a4e85affdd98ec1169cf1e89a1869a37468e131e
gurman2424 <gurmansinghbajaj24@gmail.com>
2025-05-29 11:38:12 +0530

    updated

--- src/content_option.js
+++ src/content_option.js
@@ -1,11 +1,11 @@
 const logotext = "JOHN";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "gurman singh",
+    description: "I’m gurman singh data scientist _ Full stack devloper,currently working in Berlin",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m gurman singh",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
